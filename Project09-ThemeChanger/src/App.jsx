5772caac24475cdfef108c830ef6ddf6d081bb73
vaibhavmane <151160634+vaibhavmane09@users.noreply.github.com>
2024-02-21 17:12:38 +0530

    Theme Change using Tailwind - Context APPI

---
+++ Project09-ThemeChanger/src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
+import ThemeButton from "./components/ThemeButton";
+import Card from "./components/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState('light')
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // Actual change in theme
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+
+  }, [themeMode])
+
+
+
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <h1 className="text-3xl rounded-lg text-center p-4 bg-slate-400"> Vaibhav Mane </h1>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeButton />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto"><Card /></div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
