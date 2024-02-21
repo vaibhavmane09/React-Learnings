d6562abb9cb15eeac60431ce25c83a2a8853bc3c
vaibhavmane <151160634+vaibhavmane09@users.noreply.github.com>
2024-02-21 16:15:50 +0530

    Mini Context- Basic part of Context

---
+++ MiniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,16 @@
+// Important file as we creating a context its important to create a provider file also
+
+
+import React from "react";
+import UserContext from "./UserContext";
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
+    return(
+        <UserContext.Provider value={{user, setUser}}>
+        {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider
